a9e1bf89f2e3211f58b807b099f6c5295a8b95c1
tavish5050 <tavish.42578@stu.upes.ac.in>
2026-09-12 22:38:11 +0530

    done

---
+++ D34-q2.c
@@ -0,0 +1,27 @@
+/*Delete an element from an array. */
+ #include <stdio.h> 
+ int main() 
+ { 
+    int arr[100], n, i, position; 
+    printf("Enter number of elements: "); 
+    scanf("%d", &n); 
+    printf("Enter the elements:\n"); 
+    for(i = 0; i < n; i++) 
+    { 
+        scanf("%d", &arr[i]); 
+    } 
+    printf("Enter the position to delete: "); 
+    scanf("%d", &position); 
+    // Shift elements to the left 
+    for(i = position - 1; i < n - 1; i++) 
+    { 
+        arr[i] = arr[i + 1]; 
+    } 
+    n--; 
+    printf("Array after deletion:\n"); 
+    for(i = 0; i < n; i++) 
+    { 
+        printf("%d ", arr[i]); 
+    } 
+    return 0; 
+}
