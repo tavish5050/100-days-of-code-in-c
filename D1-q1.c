e788be606dc6a73fcb8d349d660ab19db88ca17c
tavish5050 <tavish.42578@stu.upes.ac.in>
2026-09-16 08:40:39 +0530

    done

--- D1-q1.c
+++ D1-q1.c
@@ -1,1 +1,15 @@
 /*Write a program to input two numbers and display their sum*/
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
