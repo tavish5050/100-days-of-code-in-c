edcb936ca92501602dce47a88bc9b2b53cbd2db3
tavish5050 <tavish.42578@stu.upes.ac.in>
2026-09-23 20:16:12 +0530

    done

---
+++ D45-q2.c
@@ -0,0 +1,23 @@
+/*Toggle case of each character in a string. */
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 'a' + 'A';
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] - 'A' + 'a';
+        }
+    }
+
+    printf("Toggled string: %s", str);
+
+    return 0;
+}
